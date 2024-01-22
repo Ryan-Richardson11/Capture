4e46d42aad7ca7403f43c5c3d1dea11ddce0c3a4
Ryan Richardson <ryanrich811@gmail.com>
2024-01-22 15:36:56 -0500

    Added multithreading and switched path to start recording method

--- GameCapture.py
+++ GameCapture.py
@@ -1,8 +1,10 @@
+import threading
 import tkinter as tk
 import cv2
 import os
 import pyautogui
 import numpy as np
+import datetime as dt
 
 
 class GameCapture:
@@ -10,11 +12,10 @@ class GameCapture:
         self.window = window
         self.window.title("Game Capture")
         self.window.minsize(1000, 500)
-        self.save_path = os.path.join(
-            os.path.expanduser("~"), "Videos", "gameplay.mp4")
         self.recording = False
-        button_font = ("Helvetica", 12, "bold")
+        self.gameplay = None
 
+        button_font = ("Helvetica", 12, "bold")
         self.record_button = tk.Button(
             text="Record", bg="green", width=10, height=5, font=button_font, command=self.record_gameplay)
         self.record_button.pack(anchor="center", pady=10)
@@ -24,22 +25,30 @@ class GameCapture:
         self.stop_button.pack(anchor="center", pady=10)
 
     def record_gameplay(self):
-        recording = True
+        self.recording = True
         fourcc = cv2.VideoWriter_fourcc(*"mp4v")
         fps = 30.0
         resolution = (1920, 1080)
+        self.save_path = os.path.join(os.path.expanduser(
+            "~"), "Videos", f"gameplay{dt.date.today()}.mp4")
 
-        gameplay = cv2.VideoWriter(self.save_path, fourcc, fps, resolution)
+        self.gameplay = cv2.VideoWriter(
+            self.save_path, fourcc, fps, resolution)
 
-        while recording:
-            screenshot = pyautogui.screenshot()
-            cur_frame = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
-            gameplay.write(cur_frame)
+        def capture_loop():
+            while self.recording:
+                screenshot = pyautogui.screenshot()
+                cur_frame = cv2.cvtColor(
+                    np.array(screenshot), cv2.COLOR_RGB2BGR)
+                if self.gameplay:
+                    self.gameplay.write(cur_frame)
 
-        gameplay.release()
+        threading.Thread(target=capture_loop).start()
 
     def stop_recording(self):
         self.recording = False
+        if self.gameplay:
+            self.gameplay.release()
 
     def identify_game(self):
         pass
