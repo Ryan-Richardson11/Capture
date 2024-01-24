6d96be1e06f1e7fdb76962fea8a3f608af778f76
Ryan Richardson <ryanrich811@gmail.com>
2024-01-24 11:02:26 -0500

    Modified file save name in ScreenShot class to prevent overide

--- screenshot.py
+++ screenshot.py
@@ -1,15 +1,18 @@
 import os
 import pyautogui
 import time
+import datetime as dt
 
 
 class ScreenShot:
     def __init__(self):
-        self.pictures_folder = pictures_folder = os.path.join(
+        self.pictures_folder = os.path.join(
             os.path.expanduser("~"), "Pictures")
 
-    def screen_capture(self, fileName):
+    def screen_capture(self):
+        time_stamp = dt.datetime.now().strftime("%Y%m%d_%H%M%S")
         time.sleep(1)
+        fileName = f"screenshot_{time_stamp}.png"
 
         filePath = os.path.join(self.pictures_folder, fileName)
 
